9d7db3c286250f2d9a64cfd9379d175e47905dc1
Jean Luis Urena <jlurena@teladochealth.com>
2024-12-16 13:43:26 -0500

    Update github ci permissions

--- lib/active_cached_resource/version.rb
+++ lib/active_cached_resource/version.rb
@@ -3,9 +3,9 @@
 module ActiveCachedResource
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY = 1
-    PRE = "pre"
+    MINOR = 1
+    TINY = 0
+    PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
